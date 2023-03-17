0ca1db704f08a3655634a657eca4af4b041e7f87
Miguel Canessa <canessaalvamiguel@gmail.com>
2023-03-17 16:02:38 -0500

    Implement Consumer and Listener

---
+++ src/main/java/dev/canessaalvamiguel/kafkademo/KafkaListeners.java
@@ -0,0 +1,13 @@
+package dev.canessaalvamiguel.kafkademo;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaListeners {
+
+    @KafkaListener(topics = "amigoscode", groupId = "foo")
+    void listener(String data){
+        System.out.println("Listener received: " + data);
+    }
+}
